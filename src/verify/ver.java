3ebc671d674f593780005c308386fafdd563d621
sardidojoseph7 <sardidojoseph7@gmail.com>
2025-08-04 17:29:49 +0800

    ambot

---
+++ src/verify/ver.java
@@ -0,0 +1,34 @@
+
+package verify;
+package verify;
+
+import java.util.Scanner;
+
+public class ver {
+    
+    int accountNo = 10001;
+    String name;
+    float initialBalance = 0;
+    int pin = 1234;
+    
+    public boolean verifyAccount(int acc, int pn){
+        
+            if(acc == accountNo && pn == pin){
+                return true;
+            }else{
+                return false;
+            }
+    }
+    
+    public int setAccount(){
+        return accountNo;
+    }
+    
+    public void viewBalance(){
+    
+    
+    }
+    
+    
+    
+}
